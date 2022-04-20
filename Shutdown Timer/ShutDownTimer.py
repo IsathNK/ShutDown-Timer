0ff63c26cc875a716c60d4ac76572776dad04889
Isath Nenuka <gvisath@gmail.com>
2022-04-20 13:57:22 +0530

    Commit

--- Shutdown Timer/ShutDownTimer.py
+++ Shutdown Timer/ShutDownTimer.py
@@ -7,26 +7,15 @@ time = input("Give Value To Timer To Shutdown:")
 num = re.sub(r"[a-zA-Z ]", "", time) 
 unit = re.sub(r"[0-9]", "", time) 
 
-print(num)
-print(unit)
-
-
-
 #For Seconds
 if unit == "sec":
     final = int(num) * 1
 
 #For Minutes
-if unit == "mins" or " mins":
+elif unit == "mins" or " mins":
     final = int(num) * 60
 
-#For Hours
-if unit == "hour":
-    final = int(num) * 3600
-#For Days
-if unit == "days":
-    final = int(num) * 3600 * 24
+else:
+    print("Enter Valid Unit")
 
-
-
-print(final) 
+os.system('cmd /k shutdown /s /t '+ str(final))
